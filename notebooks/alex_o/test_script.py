972eda2a2298a2758338de478171186e907e8097
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2019-05-26 12:17:36 +0300

    Moved commons installing to requirements.txt (#2)

--- notebooks/alex_o/test_script.py
+++ notebooks/alex_o/test_script.py
@@ -12,8 +12,3 @@ if __name__ == "__main__":
 
     b = store_df_to_hdf_bytes(df)
     print(get_df_from_hdf_bytes(b))
-
-    from commons.ohlcv import OHLCV
-
-    df = OHLCV.fetch("BTC", "USD", "bitfinex", "1m", ohlcv_api_url="test-back")
-    print(df)
--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
+import os
 from setuptools import setup
+
+REQUIREMENTS_DIR = "requirements"
+
+
+def clean_requirements(reqs_list):
+    return [req.rstrip("\n") for req in reqs_list if req and req != "\n" and not req.startswith("#")]
+
+
+with open(os.path.join(REQUIREMENTS_DIR, "requirements.txt")) as file:
+    INSTALL_REQUIREMENTS = clean_requirements(file)
+
+
+with open(os.path.join(REQUIREMENTS_DIR, "requirements.dev.txt")) as file:
+    DEV_REQUIREMENTS = clean_requirements(file)
 
 
 setup(
     name='commons',
-    packages=['commons']
+    packages=['commons'],
+    install_requires=INSTALL_REQUIREMENTS,
+    extras_require={
+        "dev": DEV_REQUIREMENTS
+    }
 )
