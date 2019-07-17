2c97efdd857d2603fbe7356e563388be08aa0c8c
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2019-07-17 16:48:02 +0300

    Tick fixed bars feature

--- commons/features/cumulative/bars.py
+++ commons/features/cumulative/bars.py
@@ -8,13 +8,32 @@ def f1(datetimes: np.array, timeframe: int) -> pd.Series:
     :param datetimes: series of datetime
     :param timeframe: timeframe in sec.
     :return: binary series where 1 means bar generation event, 0 means bar
-    doesn't exist yet.
+    doesn't exist yet
     """
+    # TODO: Find out could we change source series shape
     index = pd.date_range(datetimes[0], datetimes[-1], freq='s')
     feature = pd.Series(np.zeros(index.size), index=index)
     feature.iloc[timeframe - 1::timeframe] = 1
     return feature[(feature == 1) | (feature.index.isin(datetimes))]
 
 
-def f2() -> pd.Series:
-    pass
+def f2(ticks: np.array, threshold: int) -> np.array:
+    """Tick fixed bars feature.
+
+    :param ticks: series of tick amounts
+    :param threshold: tick threshold
+    :return: binary series where 1 means bar generation event, 0 means bar
+    doesn't exist yet
+    """
+    bars = []
+    ticks_sum = 0
+    for tick in ticks:
+        ticks_sum += tick
+        if ticks_sum >= threshold:
+            bars.append(1)
+            ticks_sum = 0
+        else:
+            bars.append(0)
+    feature = np.array(bars)
+    assert feature.shape == ticks.shape
+    return feature
--- tests/commons/features/cumulative/test_bars.py
+++ tests/commons/features/cumulative/test_bars.py
@@ -9,14 +9,17 @@ ERROR = 1e-9
 DATASET = os.path.abspath(
     os.path.join(os.path.dirname(__file__),
                  "../../../datasets/bitfinex__BTC_USD.hdf"))
-
-
-def _volume_sum(df):
-    return df.volume_buy.sum() + df.volume_sell.sum()
-
-
-def _ticks_sum(df):
-    return df.ticks_buy.sum() + df.ticks_sell.sum()
+AGGREGATE = {
+    'open': 'first',
+    'high': 'max',
+    'low': 'min',
+    'close': 'last',
+    'volume_buy': 'sum',
+    'volume_quote_sell': 'sum',
+    'volume_quote_buy': 'sum',
+    'ticks_sell': 'sum',
+    'ticks_buy': 'sum',
+}
 
 
 @pytest.fixture
@@ -33,4 +36,29 @@ def test_f1(test_dataset):
     timedeltas = [
         td / np.timedelta64(1, 's') == 5 for td in np.diff(feature_index)
     ]
+    assert all(timedeltas)
+
+
+def test_f2(test_dataset):
+    threshold = 100
+    df = test_dataset
+    df['f2'] = features.cumulative.bars.f2(df.ticks_sell + df.ticks_buy,
+                                           threshold)
+    assert not df[df.f2 != 0].empty
+
+    new_df = df.groupby(
+        ['f2']).agg(AGGREGATE).loc[:, ['ticks_buy', 'ticks_sell']]  # yapf: disable
+    new_df.ticks = new_df.ticks_sell + new_df.ticks_buy
+    assert new_df[new_df.ticks < threshold].empty
+
+    # Compares with commons.bars
+    feature_index = df[df.f2 == 1].index.values
+    consolidated_bars = bars.tick.fixed(df, threshold, timestamp_close=True)
+    consolidated_index = consolidated_bars.timestamp_close.values
+    timedeltas = [
+        td / np.timedelta64(1, 's') == 1
+        for td in (consolidated_index - feature_index)
+    ]
+    # Because timestamp_close == timestamp of row that satisfied condition + 1s
+    # the timedelta always equals one.
     assert all(timedeltas)
