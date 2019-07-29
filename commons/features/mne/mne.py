efd4df8cb15d23029220cf18ecc434d7abb95dc9
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2019-07-29 15:02:28 +0300

    DEV-496. Added tsfresh features & tests for it

--- commons/features/mne/mne.py
+++ commons/features/mne/mne.py
@@ -1,11 +1,9 @@
-import sys
-import inspect
-from typing import List, Dict
+from typing import List
 
 import numpy as np
-import pandas as pd
 
-from .utils import rolling_window, mne_doc_helper, feature_filter
+from .utils import mne_doc_helper
+from ..utils import rolling_window, get_feature_funcs, generate_calc_all
 
 from mne_features import bivariate, univariate
 
@@ -620,47 +618,8 @@ def f32(series: np.array,
     )
 
 
-FEATURE_FUNCS = dict(inspect.getmembers(sys.modules[__name__], feature_filter))
+FEATURE_FUNCS = get_feature_funcs(__name__)
 
 __all__ = ("FEATURE_FUNCS", "PREFIX", "calc_all", *FEATURE_FUNCS.keys())
 
-
-def calc_all(
-    data: pd.DataFrame,
-    param_set: Dict[str, List[Dict]],
-    window: int,
-    column_names: dict = None
-) -> pd.DataFrame:
-    """
-    Calculate features from mne
-    :param pd.DataFrame data: df with DatetimeIndex, with `column_names.values()` columns
-    :param Dict[str, List[Dict]] param_set: keys - names of feature (f1, f2, etc.),
-        set of parameters for feature calculation, required key "column"
-    :param dict column_names: mapping of required columns
-    :return:
-    """
-    if column_names is None:
-        column_names = dict(zip(data.columns, data.columns))
-
-    vals = {name: data[column_names[orig_name]].values for orig_name, name in column_names.items()}
-
-    df = pd.DataFrame([])
-    for feature, feature_params in param_set.items():
-        for ps in feature_params:
-            column = ps.pop("column", "__all__")
-            inputs_series = vals if column == "__all__" else {column: vals[column]}
-
-            for input_series_name, input_series in inputs_series.items():
-                res = FEATURE_FUNCS[feature](series=input_series, window=window, **ps)
-
-                postfix = "_".join(f"{k}{v}" for k, v in ps.items())
-                col_name = f"{PREFIX}_{feature}_{window}_{input_series_name}{'_'if postfix else ''}{postfix}"
-
-                expected_column_size = len(input_series) - window + 1
-                if len(res) == expected_column_size:
-                    df[col_name] = res
-                else:
-                    cols = len(res) // expected_column_size
-                    for i, res in zip(range(cols), res.reshape(-1, cols).transpose()):
-                        df[f"{col_name}_col_{i}"] = res
-    return df
+calc_all = generate_calc_all(PREFIX, FEATURE_FUNCS)
