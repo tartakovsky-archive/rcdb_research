bff4a3290291d411d9b1322c33d9bada5a6bd8d1
Alex T <ksanderer@gmail.com>
2019-08-15 10:18:00 +0300

    fix: dataset transfer refactoring

--- commons/utils.py
+++ commons/utils.py
@@ -1,12 +1,12 @@
 import subprocess
 import os
-import re
 import pandas as pd
 import numpy as np
 import joblib
-
+import uuid
 from typing import List, Dict, Callable
 from sklearn.model_selection import ParameterGrid
+
 
 # from dask.distributed import Client
 # client = Client(processes=False)
@@ -55,7 +55,8 @@ def calc_all_parallel(func_calls_tasks, n_jobs=-1):
         prefix_list += [k] * len(v)
         func_calls_list += v
 
-    result = joblib.Parallel(n_jobs=n_jobs, prefer="threads")(joblib.delayed(func)(*args) for [func, args] in func_calls_list)
+    result = joblib.Parallel(n_jobs=n_jobs, prefer="threads")(
+        joblib.delayed(func)(*args) for [func, args] in func_calls_list)
 
     result_dict = dict()
     for i in range(0, len(result)):
@@ -78,7 +79,7 @@ def calc_all_parallel(func_calls_tasks, n_jobs=-1):
 
 def symlog2_(x):
     C = 0  # parameter
-    return np.sign(x)*(np.log2(1+abs(x)/(10**C)))
+    return np.sign(x) * (np.log2(1 + abs(x) / (10 ** C)))
 
 
 def custom_transform(data, p1: int, p2: str):
@@ -120,13 +121,17 @@ class Col:
         self.transforms += transforms
         return self
 
-    def get_value(self, data):
+    def get_value(self, df):
         d = None
         if self.name == "index":
-            if isinstance(data.index, pd.DatetimeIndex):
-                d = data.index.to_pydatetime()
+            if isinstance(df.index, pd.DatetimeIndex):
+                d = df.index.to_pydatetime()
+            else:
+                d = df.index.values
         else:
-            d = data[self.name].values
+            d = df[self.name].values
+
+        # d = np_get_by_col_name(self.name, data, column_names)
 
         return TransformDelayed(d, self.transforms, self.name)
 
@@ -161,15 +166,62 @@ def kwargs_to_str(kwargs, brackets=True):
     return "()"
 
 
-def fn_call_wrapper(fn, kwargs, transforms):
+df_in_process_cache = None
+
+
+def pre_cache(np_index, np_data, pd_columns):
+    global df_in_process_cache
+    if df_in_process_cache is None:
+        df_in_process_cache = pd.DataFrame(np_data, columns=pd_columns)
+        df_in_process_cache['index'] = np_index
+        df_in_process_cache.set_index("index", inplace=True)
+        return True
+    return False
+
+
+def pre_cache_hdf(hdf_path):
+    global df_in_process_cache
+    if df_in_process_cache is None:
+        df_in_process_cache = pd.read_hdf(hdf_path, key="table")
+        return True
+    return False
+
+
+def np_get_by_col_name(col_name, np_data, pd_columns):
+    try:
+        col_idx = pd_columns.index(col_name)
+    except ValueError:
+        raise ValueError(f"Column name `{col_name}` not present in dataset")
+
+    if col_idx == -1:
+        raise ValueError(f"Column name `{col_name}` not present in dataset")
+
+    return np_data[:, col_idx]
+
+
+def fn_call_wrapper(df, hdf_path, fn, kwargs, transforms):
+    if df is None:
+        print("-> pre_cache_hdf")
+        pre_cache_hdf(hdf_path)
+        global df_in_process_cache
+        df = df_in_process_cache
+
+    input_names = dict()
+    for kw_name, v in kwargs.items():
+        if type(v) == Col:
+            kwargs[kw_name] = kwargs[kw_name].get_value(df)
+            input_names[kw_name] = kwargs[kw_name].get_name()
+
     for k, v in kwargs.items():
         if type(v) == TransformDelayed:
             kwargs[k] = v.eval()
 
-    return TransformDelayed(fn(**kwargs), transforms).eval()
+    return [TransformDelayed(fn(**kwargs), transforms).eval(), input_names]
 
 
-def calc_all_config(data: pd.DataFrame, config: Dict, n_jobs=1, dask=False, verbose=False) -> pd.DataFrame:
+def calc_all_config(data: pd.DataFrame, config: Dict, n_jobs=1,
+                    dask=False, verbose=False, max_nbytes=None,
+                    dump_folder=None) -> pd.DataFrame:
     """
     Example usage:
 
@@ -207,10 +259,16 @@ def calc_all_config(data: pd.DataFrame, config: Dict, n_jobs=1, dask=False, verb
     :return:
     """
 
+    if dump_folder is not None and dask:
+        raise AttributeError("`dask` mode can be used with remote cluster, `dump_folder` should be None`")
+
     joblib_client = "loky"
     if dask:
         joblib_client = "dask"
+        # disable max_nbytes while in dask (can be remote cluster)
+        max_nbytes = None
 
+    print("joblib_client", joblib_client)
     with joblib.parallel_backend(joblib_client, n_jobs=n_jobs):
         fn_parallel_list = []
         for prefix, fn_settings_list in config.items():
@@ -234,42 +292,83 @@ def calc_all_config(data: pd.DataFrame, config: Dict, n_jobs=1, dask=False, verb
                                 # automatic "col_name" -> km.col("col_name")
                                 kwargs[kw_name] = km.col(kwargs[kw_name])
 
-                            kwargs[kw_name] = kwargs[kw_name].get_value(data)
-                            kwargs_dump['inputs'][kw_name] = kwargs[kw_name].get_name()
+                            # passed to fn_call_wrapper
+                            # kwargs[kw_name] = kwargs[kw_name].get_value(data)
+                            # kwargs_dump['inputs'][kw_name] = kwargs[kw_name].get_name()
 
                     fn_parallel_list.append([fn, kwargs, transforms_post, dict(prefix=prefix, kwargs=kwargs_dump)])
 
-        results = joblib.Parallel(
-            # n_jobs=n_jobs,
-            # prefer="threads" # usually it's a bad idea, but may be useful for future
-        )(
-            joblib.delayed(fn_call_wrapper)(func, kwargs, transforms) for [func, kwargs, transforms, _] in fn_parallel_list
-        )
+        with joblib.Parallel(
+                max_nbytes=max_nbytes,
+                n_jobs=n_jobs,
+                # prefer="threads" # usually it's a bad idea, but may be useful for future
+        ) as parallel:
+            # cache data on workers
 
-        result_dict = dict()
-        i = 0
-        for [fn, _, transforms_post, dump] in fn_parallel_list:
-            prefix = dump['prefix']
-            kwargs_dump = dump['kwargs']
-            transforms_post_str = ".".join([t.get_name() for t in transforms_post])
+            # print(parallel.n_jobs, parallel.)
+            # tasks_count = n_jobs if n_jobs != -1 else joblib.cpu_count()
+            # cache_res = parallel(
+            #     [joblib.delayed(pre_cache)(data.index, data.values, data.columns) for i in range(tasks_count)]
+            # )
+            #
+            # print("cache_res", cache_res)
 
-            fn_name = fn.__name__
-            kwargs_str = [kwargs_to_str(kwargs_dump['inputs'], brackets=False)]
-            if kwargs_dump['params']:
-                kwargs_str.append(kwargs_to_str(kwargs_dump['params'], brackets=False))
+            # np_columns = list(data.columns)
+            # np_data = data.values
+            # np_index = data.index
+            # if isinstance(np_index, pd.DatetimeIndex):
+            #     np_index = np_index.to_pydatetime()
+            # # append index as last column
+            # # print(np_data[:, :-1])
+            # # print(np_index)
+            # np_data = np.c_[np_index, np_data]
+            # np_columns = ['index'] + np_columns
 
-            kwargs_str = ", ".join(kwargs_str)
-            result_name = f"{prefix}.{fn_name}({kwargs_str})"
-            if transforms_post_str:
-                result_name += f".{transforms_post_str}"
+            data_file_name = None
+            data_file_path = None
+            if dump_folder:
+                data_file_name = uuid.uuid4()
+                data_file_path = f"{dump_folder}/{data_file_name}"
+                data.to_hdf(data_file_path, key="table")
 
-            # TODO: @multiple_outputs_task if results[i] is not 1D np.array we can split it to
-            #       multiple outputs with `_{i}` postfix
+            try:
+                df_data = None
+                if not data_file_path:
+                    df_data = data
 
-            result_dict[result_name] = results[i]
-            i += 1
+                results = parallel(
+                    joblib.delayed(fn_call_wrapper)(df_data, data_file_path, func, kwargs, transforms) for [func, kwargs, transforms, _] in
+                    fn_parallel_list
+                )
 
-        return pd.DataFrame(result_dict)
+                result_dict = dict()
+                i = 0
+                for [fn, _, transforms_post, dump] in fn_parallel_list:
+                    prefix = dump['prefix']
+                    kwargs_dump = dump['kwargs']
+                    transforms_post_str = ".".join([t.get_name() for t in transforms_post])
+
+                    fn_name = fn.__name__
+                    kwargs_str = [kwargs_to_str(kwargs_dump['inputs'], brackets=False)]
+                    if kwargs_dump['params']:
+                        kwargs_str.append(kwargs_to_str(kwargs_dump['params'], brackets=False))
+
+                    kwargs_str = ", ".join(kwargs_str)
+                    result_name = f"{prefix}.{fn_name}({kwargs_str})"
+                    if transforms_post_str:
+                        result_name += f".{transforms_post_str}"
+
+                    # TODO: @multiple_outputs_task if results[i] is not 1D np.array we can split it to
+                    #       multiple outputs with `_{i}` postfix
+
+                    result_dict[result_name] = results[i]
+                    i += 1
+
+            finally:
+                if data_file_path:
+                    os.remove(data_file_path)
+
+            return pd.DataFrame(result_dict)
 
 
 class KeyMap:
