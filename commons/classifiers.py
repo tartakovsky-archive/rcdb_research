9dab629bff72241664ae6298a383f1dab89988f8
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2019-09-17 16:03:26 +0300

    Add classifiers.py with ThresholdClassifier

---
+++ commons/classifiers.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.base import ClassifierMixin, BaseEstimator
+
+class ThresholdClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, clf, threshold):
+        """Replaces all features with `clf.predict_proba(X)`"""
+        self.clf = clf
+        self.threshold = threshold
+
+    def fit(self, X, y):
+        self.clf.fit(X, y)
+        return self
+
+    def predict(self, X):
+        predicts = self.clf.predict_proba(X)[:, 1]
+        predicts = np.where(predicts < self.threshold, 0, 1)
+        return predicts
+
+    def score(self, X, y, sample_weight=None):
+        return self.clf.score(X, y, sample_weight)
+
+    def get_params(self, deep=True):
+        params = super().get_params(deep=deep)
+        params['threshold'] = self.threshold
+        params['clf'] = self.clf
+        return params
