797c4617c2e2005ea80cf89247ea7e4e8204ec87
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2019-10-02 16:28:36 +0300

    DEV-587. Added tests and direction tests fixes

--- tests/commons/features/test_diff.py
+++ tests/commons/features/test_diff.py
@@ -1,34 +1,22 @@
+import pytest
 import numpy as np
 
 from commons.features import diff
 
-
 TEST_SERIES = np.array([1., 2., 12., 5., 5.5, 2.])
 
 
-def test_diff():
-    res = diff.diff(TEST_SERIES, 5)
+@pytest.mark.parametrize(
+    'func, params, nan_offset',
+    [
+        (diff.diff, (TEST_SERIES, 5), 5),
+        (diff.frac_diff, (TEST_SERIES, 5), 5),
+        (diff.series_ma_frac_diff, (TEST_SERIES, 5), 4),
+        (diff.two_series_ma_frac_diff, (TEST_SERIES, TEST_SERIES * 2, 5), 4),
+    ]
+)
+def test_diffs(func, params, nan_offset):
+    res = func(*params)
 
-    assert np.isnan(res[:5]).all()
-    assert np.isfinite(res[5:]).all()
-
-
-def test_frac_diff():
-    res = diff.frac_diff(TEST_SERIES, 5)
-
-    assert np.isnan(res[:5]).all()
-    assert np.isfinite(res[5:]).all()
-
-
-def test_series_ma_frac_diff():
-    res = diff.series_ma_frac_diff(TEST_SERIES, 5)
-
-    assert np.isnan(res[:4]).all()
-    assert np.isfinite(res[4:]).all()
-
-
-def test_two_series_ma_frac_diff():
-    res = diff.two_series_ma_frac_diff(TEST_SERIES, TEST_SERIES * 2, 5)
-
-    assert np.isnan(res[:4]).all()
-    assert np.isfinite(res[4:]).all()
+    assert np.isnan(res[:nan_offset]).all()
+    assert np.isfinite(res[nan_offset:]).all()
--- tests/commons/features/test_direction.py
+++ tests/commons/features/test_direction.py
@@ -9,16 +9,6 @@ TEST_DF = pd.DataFrame({"close": TEST_ARR})
 WINDOW_SIZE = 3
 TEST_RES = [np.nan, np.nan, 2, 1, 1, -1, -1]
 
-PARAM_SET = {
-    "sum_of_direction": [{"column": "close"}]
-}
-
-
-def test_calc_all():
-    res = direction.calc_all(TEST_DF, PARAM_SET, WINDOW_SIZE)
-    assert len(res) == len(TEST_DF)
-    assert hasattr(res, f"{direction.PREFIX}_sum_of_direction_3_close")
-
 
 def test_sum_of_direction():
     res = direction.sum_of_direction(TEST_ARR, WINDOW_SIZE)
