d76a6f9fae4cd48377863927c3110185754f7719
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2019-12-09 12:57:30 +0300

    Feature/dev 609 multilinesplitter (#103)
    * DEV-609. MultiInputSplitter
    
    * added `drop_earlier_history` parameter to MultiInputSplitter
    
    * spliter input/output format changes
    
    * cv import fixes
    
    * splitter fix output format
    
    * aggregate_splits
    
    * flake fixes
    
    * flake fixes
    
    * Fix aggregate_splits, add predict_aggregated_splits
    
    * add params to agg test param
    
    * agg learning. docstring & test for agg predict

---
+++ rcdb_research/aggregated_learning.py
@@ -0,0 +1,192 @@
+from typing import List, Tuple
+from joblib import Parallel, delayed
+from sklearn.base import clone, BaseEstimator
+
+import numpy as np
+import pandas as pd
+
+from .cross_validators import WalkForwardCV
+
+
+class MultiInputSplitter(WalkForwardCV):
+    """
+    Multi input version of WalkForwardCV.
+    Initialization:
+    :param n_splits: count of splits
+    :param test_size: test set size
+    :param train_size: train set size
+    :param gap_size: gap size
+    :param is_fixed: if True then use sizes (train_size, test_size, gap_size, ) as count of items
+    :param drop_earlier_history: if True then cut history by base dataset
+    """
+
+    def __init__(
+        self,
+        n_splits,
+        test_size=None,
+        train_size=None,
+        gap_size=0,
+        is_fixed=False,
+        drop_earlier_history=False
+    ):
+        super().__init__(
+            n_splits,
+            test_size=test_size,
+            train_size=train_size,
+            gap_size=gap_size,
+            is_fixed=is_fixed,
+            expanding=True
+        )
+        self.drop_earlier_history = drop_earlier_history
+
+    def split(
+        self,
+        main: Tuple[pd.DataFrame, pd.Series],
+        additional: List[Tuple[pd.DataFrame, pd.Series]] = tuple(),
+        verbose: bool = False
+    ) -> List[dict]:
+        """
+        split method
+        :param main: general dataset, tuple of X and y
+        :param additional: additional datasets, list of tuple of X and y
+        :param verbose: print splits
+        """
+        X_main, y_main = main
+        index = X_main.index
+
+        splits = []
+        for train_idx_poses, test_idx_poses in super().split(index):
+            train_start, train_end = index[train_idx_poses[0]], index[train_idx_poses[-1]]
+            test_start, test_end = index[test_idx_poses[0]], index[test_idx_poses[-1]]
+            if verbose:
+                print(f'train {len(splits)} {train_start}:{train_end}')
+                print(f'test {len(splits)} {test_start}:{test_end}')
+
+            Xs_additional, ys_additional = [], []
+            for set_idx, train_set in enumerate(additional):
+                X, y = train_set
+                if self.drop_earlier_history:
+                    Xs_additional.append(X[(train_start <= X.index) & (X.index <= train_end)])
+                    ys_additional.append(y[(train_start <= y.index) & (y.index <= train_end)])
+                else:
+                    Xs_additional.append(X[X.index <= train_end])
+                    ys_additional.append(y[y.index <= train_end])
+
+                if verbose:
+                    print('=' * 10, 'Split', len(splits), '=' * 10)
+                    print('Train', len(splits))
+                    print(f'X_train{set_idx}')
+                    print(Xs_additional[-1])
+                    print(f'y_train{set_idx}')
+                    print(ys_additional[-1])
+
+            if self.drop_earlier_history:
+                X_train_split = X_main[(train_start <= X_main.index) & (X_main.index <= train_end)]
+                y_train_split = y_main[(train_start <= y_main.index) & (y_main.index <= train_end)]
+            else:
+                X_train_split = X_main[X_main.index <= train_end]
+                y_train_split = y_main[y_main.index <= train_end]
+
+            X_test_split = X_main[(test_start <= X_main.index) & (X_main.index <= test_end)]
+            y_test_split = y_main[(test_start <= y_main.index) & (y_main.index <= test_end)]
+
+            if verbose:
+                print('Test main', len(splits))
+                print('X_train')
+                print(X_train_split)
+                print('y_train')
+                print(y_train_split)
+                print()
+                print('Test main', len(splits))
+                print('X_test')
+                print(X_test_split)
+                print('y_test')
+                print(y_test_split)
+                print('=' * 10, 'End Split', len(splits), '=' * 10)
+
+            splits.append(
+                dict(
+                    main=dict(
+                        X_train=X_train_split,
+                        y_train=y_train_split,
+                        X_test=X_test_split,
+                        y_test=y_test_split
+
+                    ),
+                    additional=[dict(X_train=x, y_train=y) for x, y in zip(Xs_additional, ys_additional)]
+                )
+            )
+
+        return splits
+
+
+def aggregate_splits(splits: List[dict], pre_agg_transforms: 'BaseEstimator') -> List[Tuple]:
+    """
+    Aggregate splits data by some estimator
+    :param splits: splits from MultiInputSplitter
+    :param pre_agg_transforms: estimator for agg transformation
+    :returns: aggregated splits
+    """
+    aggregated_splits = []
+
+    for split in splits:
+        # Parse splits
+        main_dataset, additional_datasets = split.values()
+        X_train, y_train, X_test, y_test = [df.values for df in main_dataset.values()]
+        add_X_trains = [d['X_train'].values for d in additional_datasets]
+        add_y_trains = [d['y_train'].values for d in additional_datasets]
+
+        # Pre-transform main dataset
+        pre_agg_transforms.fit(X_train)
+        X_train = pre_agg_transforms.transform(X_train)
+        X_test = pre_agg_transforms.transform(X_test)
+
+        agg_X_trains = [X_train]
+        agg_y_trains = [y_train]
+
+        if len(add_X_trains) > 0:
+            # Pre-transform additional datasets
+            for i, X_tr in enumerate(add_X_trains):
+                if X_tr.size > 0:
+                    add_X_trains[i] = pre_agg_transforms.fit_transform(X_tr)
+
+            agg_X_trains.append(np.vstack(add_X_trains))
+            agg_y_trains.append(np.hstack(add_y_trains))
+
+        # Aggregate
+        aggregated_splits.append((
+            np.vstack(agg_X_trains),
+            np.hstack(agg_y_trains),
+            X_test,
+            y_test,
+        ))
+
+    return aggregated_splits
+
+
+def predict_aggregated_splits(clf: 'BaseEstimator', agg_splits: List[Tuple],
+                              n_jobs: int = -1) -> Tuple[np.array, np.array]:
+    """
+    Aggregated splits prediction
+    :param cls: predictor
+    :agg_splits: aggregated splits
+    :param n_jobs: count of jobs for  joblib.Parallel
+    :returns: tuple of y predicted and true y
+    """
+    def predict_split(clf, split):
+        X_train, y_train, X_test, y_test = split
+
+        y_true = y_test
+
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+
+        return y_true, y_pred
+
+    parallel = Parallel(n_jobs=n_jobs)
+    prediction_blocks = parallel(
+        delayed(predict_split)(clone(clf), split) for split in agg_splits
+    )
+
+    y_trues, y_preds = zip(*prediction_blocks)
+    return np.concatenate(y_trues), np.concatenate(y_preds)
---
+++ tests/rcdb_research/test_aggregated_learning.py
@@ -0,0 +1,133 @@
+import pytest
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.tree import DecisionTreeClassifier
+
+from rcdb_research.aggregated_learning import \
+    MultiInputSplitter, aggregate_splits, predict_aggregated_splits
+
+
+X = pd.DataFrame(
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+    columns=['X'],
+    index=pd.date_range('2019-11-15', periods=11)
+)
+y = pd.Series(
+    [0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0],
+    index=pd.date_range('2019-11-15', periods=11)
+)
+
+X1 = pd.DataFrame(
+    -np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]),
+    columns=['X1'],
+    index=pd.DatetimeIndex(['2019-11-14', '2019-11-15', '2019-11-17', '2019-11-17', '2019-11-18',
+                            '2019-11-19', '2019-11-20', '2019-11-21', '2019-11-22',
+                            '2019-11-23', '2019-11-24', '2019-11-25'])
+)
+y1 = pd.Series(
+    -np.array([1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0]),
+    index=pd.DatetimeIndex(['2019-11-14', '2019-11-15', '2019-11-17', '2019-11-17', '2019-11-18',
+                            '2019-11-19', '2019-11-20', '2019-11-21', '2019-11-22',
+                            '2019-11-23', '2019-11-24', '2019-11-25'])
+)
+
+
+@pytest.mark.parametrize(
+    'drop_earlier_history, empty_additional',
+    [
+        [True, False],
+        [True, True],
+        [False, True],
+        [False, False],
+    ]
+)
+def test_MultiInputSplitter_split(drop_earlier_history, empty_additional):
+    print('drop_earlier_history', drop_earlier_history)
+
+    if not empty_additional:
+        splits = MultiInputSplitter(3, train_size=0.75, drop_earlier_history=drop_earlier_history).split(
+            main=(X, y),
+            additional=[(X1, y1)],
+            verbose=True,
+        )
+    else:
+        splits = MultiInputSplitter(3, train_size=0.75, drop_earlier_history=drop_earlier_history).split(
+            main=(X, y),
+            verbose=True,
+        )
+
+    assert len(splits) == 3
+    for split in splits:
+        X_test = split['main']['X_test']
+        y_test = split['main']['y_test']
+
+        assert y_test.index.equals(X_test.index)
+
+        main_train = [
+            dict(
+                X_train=split['main']['X_train'],
+                y_train=split['main']['y_train']
+            )
+        ]
+        for additional in split['additional'] + main_train:
+            X_train = additional['X_train']
+            y_train = additional['y_train']
+
+            assert y_train.index.equals(X_train.index)
+
+            assert (X_train.index < X_test.index[0]).all()
+
+            if not empty_additional:
+                if drop_earlier_history:
+                    assert X.index[0] <= X_train.index[0]
+                else:
+                    assert X1.index[0] <= X_train.index[0]
+
+
+@pytest.mark.parametrize(
+    'empty_additional', [True, False]
+)
+def test_aggregate_splits(empty_additional):
+    if not empty_additional:
+        splits = MultiInputSplitter(3, train_size=0.75).split(
+            main=(X, y),
+            additional=[(X1, y1)],
+        )
+    else:
+        splits = MultiInputSplitter(3, train_size=0.75).split(
+            main=(X, y),
+        )
+    res = aggregate_splits(
+        splits=splits, pre_agg_transforms=MinMaxScaler()
+    )
+    assert len(res) == 3
+    for agg_split, split in zip(res, splits):
+        assert len(agg_split) == 4
+        assert agg_split[0].size == \
+            split['main']['X_train'].values.size + sum(x['X_train'].size for x in split['additional'])
+        assert agg_split[1].size == \
+            split['main']['y_train'].values.size + sum(x['y_train'].size for x in split['additional'])
+
+        assert agg_split[2].size == split['main']['X_test'].values.size
+        assert (agg_split[3] == split['main']['y_test'].values).all()
+
+
+@pytest.mark.parametrize(
+    'n_jobs', [1, -1, 2]
+)
+def test_predict_aggregated_splits(n_jobs):
+    splits = MultiInputSplitter(3, train_size=0.75).split(
+        main=(X, y),
+        additional=[(X1, y1)],
+    )
+    agg_splits = aggregate_splits(
+        splits=splits, pre_agg_transforms=MinMaxScaler()
+    )
+    res = predict_aggregated_splits(
+        clf=DecisionTreeClassifier(),
+        agg_splits=agg_splits,
+        n_jobs=n_jobs
+    )
+    assert len(res) == 2
+    assert res[0].shape[0] == res[1].shape[0]
