1187162a23be89d5644726159dcc22462d1b27ea
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2019-12-11 17:16:34 +0300

    Add QuantileClassifier (#107)

--- rcdb_research/classifiers.py
+++ rcdb_research/classifiers.py
@@ -25,3 +25,31 @@ class ThresholdClassifier(BaseEstimator, ClassifierMixin):
         params['threshold'] = self.threshold
         params['clf'] = self.clf
         return params
+
+
+class QuantileClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, clf, q, quantile=0.5):
+        self.clf = clf
+        self.q = q
+        self.quantile = quantile
+
+    def fit(self, X, y):
+        self.clf.fit(X, y)
+        y_train_probas = self.clf.predict_proba(X)[:, 1]
+        self.quantile = np.quantile(y_train_probas, self.q)
+        return self
+
+    def predict(self, X):
+        y_test_probas = self.clf.predict_proba(X)[:, 1]
+        predicts = np.where(y_test_probas > self.quantile, 1, 0)
+        return predicts
+
+    def score(self, X, y, sample_weight=None):
+        return self.clf.score(X, y, sample_weight)
+
+    def get_params(self, deep=True):
+        params = super().get_params(deep=deep)
+        params['clf'] = self.clf
+        params['q'] = self.q
+        params['quantile'] = self.quantile
+        return params
