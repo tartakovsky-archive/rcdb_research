19ba9cd1d670015c357fbcdbb5237b36063937db
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-02-06 22:51:43 +0300

    Add PositionSizing / Fee / Slippage / MarketImpact static classes

---
+++ rcdb_research/simulation/simulators/comissions.py
@@ -0,0 +1,30 @@
+from typing import Callable
+
+
+class PositionSizing:
+    @staticmethod
+    def flat(units: float) -> Callable:
+
+        def sizing_fn() -> float:
+            return units
+
+        return sizing_fn
+
+    @staticmethod
+    def percent(percent: float) -> Callable:
+
+        def sizing_fn(current_equity: float) -> float:
+            return current_equity * percent
+
+        return sizing_fn
+
+    @staticmethod
+    def kelly(win_size: float, loss_size: float, divider: float = 1) -> Callable:
+
+        def sizing_fn(current_equity: float, win_proba: float) -> float:
+            kelly = win_proba / loss_size - (1 - win_proba) / win_size
+            kelly = kelly / divider
+
+            return current_equity
+
+        return sizing_fn
