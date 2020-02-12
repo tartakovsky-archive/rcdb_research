57b708f2675e6a66f48211abd32a6ddd13130643
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-02-12 17:37:57 +0300

    Update Voting imports

--- rcdb_research/simulation/__init__.py
+++ rcdb_research/simulation/__init__.py
@@ -1,3 +1,3 @@
 from .entities import Probabilities, Predictions, Trades  # noqa
 from .metrics import ProbabilityMetrics, PredictionMetrics, TradingMetrics  # noqa
-from .simulators import PredictionSimulator, VotingSimulator, TradingSimulator  # noqa
+from .simulators import PredictionSimulator, Voting, TradingSimulator  # noqa
