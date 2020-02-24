ac2c68b04b9684d881dadda2126a948a2528aa8f
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-02-25 00:30:28 +0300

    Map models to str, dict and df

--- rcdb_research/simulation/trades/models.py
+++ rcdb_research/simulation/trades/models.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 from typing import NamedTuple, Optional
 from enum import Enum
 
+import pandas as pd
+
 
 class Exchange(NamedTuple):
     name: str
@@ -48,6 +50,16 @@ class EquityState(NamedTuple):
     def exposure(self) -> float:
         return self.base_holdings_quote_value / self.equity
 
+    def __str__(self):
+        params = str(self.to_dict()).strip("{}").replace(': ', '=').replace("'", '')
+        return f"{self.__class__.__name__}({params})"
+
+    def to_dict(self, prefix=""):
+        d = dict(self._asdict())
+        d.update({'equity': self.equity, 'exposure': self.exposure})
+        d = {prefix + k: v for (k, v) in d.items()}
+        return d
+
 
 class OrderType(Enum):
     market = 'market'
@@ -61,11 +73,33 @@ class Order(NamedTuple):
     price: float
     size: float
 
+    def __str__(self):
+        params = str(self.to_dict()).strip("{}").replace(': ', '=').replace("'", '')
+        return f"{self.__class__.__name__}({params})"
+
+    def to_dict(self, prefix=""):
+        return {
+            f"{prefix}type": self.type.value,
+            f"{prefix}exchange": self.exchange.value.name,
+            f"{prefix}pair": str(self.pair),
+            f"{prefix}price": self.price,
+            f"{prefix}size": self.size
+        }
+
 
 class TradeSignal(NamedTuple):
     proba: float
     desired_price: float
     desired_exposure: float
+
+    def __str__(self):
+        params = str(self.to_dict()).strip("{}").replace(': ', '=').replace("'", '')
+        return f"{self.__class__.__name__}({params})"
+
+    def to_dict(self, prefix=""):
+        d = dict(self._asdict())
+        d = {prefix + k: v for (k, v) in d.items()}
+        return d
 
 
 class ExecutionResult(NamedTuple):
@@ -74,6 +108,20 @@ class ExecutionResult(NamedTuple):
     base_change: float
     quote_change: float
 
+    def __str__(self):
+        order_params = str(self.order.to_dict()).strip("{}").replace(': ', '=').replace("'", '')
+        order_str = f"{self.order.__class__.__name__}({order_params})"
+        d = self.to_dict()
+        d['order'] = order_str
+        params = str(d).strip("{}").replace(': ', '=').replace("'", '')
+        return f"{self.__class__.__name__}({params})"
+
+    def to_dict(self, prefix=""):
+        d = dict(self._asdict())
+        d['order'] = d['order'].to_dict()
+        d = {prefix + k: v for (k, v) in d.items()}
+        return d
+
 
 class BarContext(NamedTuple):
     pre_trade_state: EquityState
@@ -81,6 +129,43 @@ class BarContext(NamedTuple):
     order: Optional[Order]
     execution_result: Optional[ExecutionResult]
     post_trade_state: EquityState
+
+    def __str__(self):
+        order_params = str(self.order.to_dict()).strip("{}").replace(': ', '=').replace("'", '')
+        order_str = f"{self.order.__class__.__name__}({order_params})"
+        d = self.to_dict()
+        d['order'] = order_str
+        params = str(d).strip("{}").replace(': ', '=').replace("'", '')
+        return f"{self.__class__.__name__}({params})"
+
+    def to_dict(self, prefix=""):
+        d = dict(self._asdict())
+
+        d = {prefix + k: v.to_dict() for (k, v) in d.items() if v is not None}
+
+        return d
+
+    def to_df(self):
+        d = dict(self._asdict())
+
+        nd = {}
+        nd.update(d['pre_trade_state'].to_dict(prefix="pre_"))
+
+        trade_signal = d.get('trade_signal', None)
+        if trade_signal is not None:
+            nd.update(trade_signal.to_dict())
+
+        order = d.get('order', None)
+        if order is not None:
+            nd.update({(f"order_{k}" if k == 'type' else k): v for (k, v) in order.to_dict().items()})
+
+        execution_result = d.get('execution_result', None)
+        if execution_result is not None:
+            nd.update({k: v for (k, v) in execution_result.to_dict().items() if k != 'order'})
+
+        nd.update(d['post_trade_state'].to_dict(prefix="post_"))
+
+        return pd.DataFrame([nd])
 
 
 # Enums
