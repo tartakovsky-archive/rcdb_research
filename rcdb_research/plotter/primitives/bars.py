1505128d84d84b0146a37625ccc973e7d436e421
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-04-17 22:37:24 +0300

    bars: add bar_kwargs

--- rcdb_research/plotter/primitives/bars.py
+++ rcdb_research/plotter/primitives/bars.py
@@ -16,11 +16,15 @@ def bars(y: np.array, x: np.array = None, threshold: float = 0,
          title: Optional[str] = None, xlabel: Optional[str] = None, ylabel: Optional[str] = None,
          colors: Optional[dict] = None, legend: Optional[dict] = None,
          fig_kwargs: Optional[dict] = None, ax_kwargs: Optional[dict] = None,
+         bar_kwargs: Optional[dict] = None,
          ax=None):
     colors = colors or bars_colors()
     legend = legend or bars_legend()
     fig_kwargs = fig_kwargs or style.fig_kwargs()
     ax_kwargs = ax_kwargs or style.ax_kwargs()
+    bar_kwargs = bar_kwargs or style.line_kwargs()
+    _ = bar_kwargs.pop('color', None)
+    _ = bar_kwargs.pop('label', None)
 
     x = np.arange(y.size) if x is None else x
 
@@ -29,11 +33,13 @@ def bars(y: np.array, x: np.array = None, threshold: float = 0,
 
     if np.any(y >= threshold):
         y_pos = np.where(y >= threshold, y - threshold, np.nan)
-        axis.bar(x, height=y_pos, bottom=threshold, color=colors['pos'], label=legend['pos'])
+        axis.bar(x, height=y_pos, bottom=threshold,
+                 color=colors['pos'], label=legend['pos'], **bar_kwargs)
 
     if np.any(y < threshold):
         y_neg = np.where(y < threshold, y - threshold, np.nan)
-        axis.bar(x, height=y_neg, bottom=threshold, color=colors['neg'], label=legend['neg'])
+        axis.bar(x, height=y_neg, bottom=threshold,
+                 color=colors['neg'], label=legend['neg'], **bar_kwargs)
 
     if ax is None:
         return fig, axis
