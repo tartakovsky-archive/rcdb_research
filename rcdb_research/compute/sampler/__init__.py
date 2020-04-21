21612b76ab98803f8883049dafc3149c9f84c111
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-04-21 14:33:31 +0300

    Add imports

--- rcdb_research/compute/sampler/__init__.py
+++ rcdb_research/compute/sampler/__init__.py
@@ -1,1 +1,4 @@
-from .sampler import Sampler  # noqa
+from .distributions import Sampler  # noqa
+from .distributions import Uniform, LogUniform, QuantizedUniform, QuantizedLogUniform  # noqa
+from .distributions import Normal, LogNormal, QuantizedNormal, QuantizedLogNormal  # noqa
+from .distributions import DiscreteUniform, DiscreteNormal, TruncatedNormal  # noqa
