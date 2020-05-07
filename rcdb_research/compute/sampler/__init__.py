539e7f1f3e55ab62ff5a7bbad5f86198eacae850
Eugene Tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-05-07 20:58:20 +0300

    fix sampler imports

--- rcdb_research/compute/sampler/__init__.py
+++ rcdb_research/compute/sampler/__init__.py
@@ -1,4 +1,1 @@
 from .distributions import Sampler  # noqa
-from .distributions import Uniform, LogUniform, QuantizedUniform, QuantizedLogUniform  # noqa
-from .distributions import Normal, LogNormal, QuantizedNormal, QuantizedLogNormal  # noqa
-from .distributions import DiscreteUniform, DiscreteNormal, TruncatedNormal  # noqa
