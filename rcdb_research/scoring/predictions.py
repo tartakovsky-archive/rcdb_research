d608126c7b26c33b76973f4cc6df26caf1511bd0
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-05-12 00:46:30 +0300

    scoring module: refactor

--- rcdb_research/scoring/predictions.py
+++ rcdb_research/scoring/predictions.py
@@ -2,16 +2,22 @@ from typing import List, Dict, Callable
 import numpy as np
 
 
-def score_path_1d(data: Dict[str, np.ndarray],
-                  score: Callable[[np.ndarray, np.ndarray], float]) -> float:
-    return score(data['y_true'], data['y_pred'])
+def score_path(data: Dict[str, np.ndarray],
+               fn: Callable[[np.ndarray, np.ndarray], float]) -> float:
+    return fn(data['y_true'], data['y_pred'])
 
 
 def score_path_2d(data: List[Dict[str, np.ndarray]],
-                  score: Callable[[np.ndarray, np.ndarray], float]) -> np.ndarray:
-    return np.array([score_path_1d(dct, score) for dct in data])
+                  fn: Callable[[np.ndarray, np.ndarray], float]) -> List[float]:
+    return [
+        fn(d['y_true'], d['y_pred'])
+        for d in data
+    ]
 
 
 def score_path_3d(data: List[List[Dict[str, np.ndarray]]],
-                  score: Callable[[np.ndarray, np.ndarray], float]) -> np.ndarray:
-    return np.array([score_path_2d(lst, score) for lst in data])
+                  fn: Callable[[np.ndarray, np.ndarray], float]) -> List[List[float]]:
+    return [
+        [fn(d['y_true'], d['y_pred']) for d in dicts]
+        for dicts in data
+    ]
