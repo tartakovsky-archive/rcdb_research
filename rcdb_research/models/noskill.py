53cba47dc8bd93178d36d2e2c1a3ac553f8a7e5d
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-05-16 00:08:06 +0300

    NoSkillClassifier review and adjustments

--- rcdb_research/models/noskill.py
+++ rcdb_research/models/noskill.py
@@ -1,26 +1,17 @@
-import numpy as np
+# import numpy as np
 
 from functools import partial
+# from collections import Counter
 
 from sklearn.dummy import DummyClassifier
 from sklearn.base import ClassifierMixin, BaseEstimator
 
-
-class LeastFrequentClassifier(DummyClassifier):
-    def __init__(self):
-        super().__init__(strategy='constant')
-
-    def fit(self, X, y, sample_weight=None):
-        self.constant = np.argmin(np.bincount(y))
-        return super().fit(X, y, sample_weight)
-
-
 SCORE_CLASSIFIER_MAPPING = dict(
     accuracy=partial(DummyClassifier, strategy='most_frequent'),
-    precision=partial(LeastFrequentClassifier),
-    f=partial(LeastFrequentClassifier),
-    auc=partial(DummyClassifier, strategy='stratified'),
     pwa=partial(DummyClassifier, strategy='most_frequent'),
+    f=partial(DummyClassifier, strategy='stratified'),
+    roc_auc=partial(DummyClassifier, strategy='stratified'),
+    avg_prec=partial(DummyClassifier, strategy='stratified'),
     neg_log_loss=partial(DummyClassifier, strategy='prior')
 )
 
@@ -32,6 +23,8 @@ class NoSkillClassifier(BaseEstimator, ClassifierMixin):
         self.clf = SCORE_CLASSIFIER_MAPPING[score]()
 
     def fit(self, X, y, *args, **kwargs):
+        # if self.clf.strategy == 'constant':
+        #     self.clf.constant = Counter(y).most_common()[-1][0]
         return self.clf.fit(X, y, *args, **kwargs)
 
     def predict(self, X, *args, **kwargs):
