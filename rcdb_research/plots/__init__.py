db5bdc0c2a341105587a29d66394151cb268e056
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-05-21 20:35:35 +0300

    clean up unused code

--- rcdb_research/plots/__init__.py
+++
@@ -1,3 +0,0 @@
-from .primitives import curve, bars, area, histogram, cmap_gradient, second_index, configure_axis  # noqa
-from .reports import proba_report, preds_report, trading_report, threshold_report  # noqa
-from .style import Style, colormap  # noqa
