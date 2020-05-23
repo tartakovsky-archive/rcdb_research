bab895d3c170462e3812e8becd7c5b9e606fbb22
Ilya Pikulin <ilya.pikulin@gmail.com>
2020-05-23 21:44:42 +0300

    Squash mc_sizing_cpp/ and mc_sizing/ into one dir

--- setup.py
+++ setup.py
@@ -43,10 +43,10 @@ if prefix_dev:
 # install mc_sizing
 if 'egg_info' in sys.argv:
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    source_dir = os.path.join(base_dir, module_name, 'sizing', 'mc_sizing_cpp')
+    source_dir = os.path.join(base_dir, module_name, 'sizing', 'mc_sizing')
     cmd(['make'], cwd=source_dir)
     dist_path = os.path.join(base_dir, module_name, 'sizing', 'mc_sizing')
-    copy2(os.path.join(source_dir, 'lib.so'), dist_path)
+    # copy2(os.path.join(source_dir, 'lib.so'), dist_path)
 
 
 setup(
