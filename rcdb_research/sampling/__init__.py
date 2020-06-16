7f3bc52d7d9c5a7c8939225f5101ae7b7781d6b4
Ilya Pikulin <ilya.pikulin@gmail.com>
2020-06-17 00:21:43 +0300

    Implement TB and SB in C++

--- rcdb_research/sampling/__init__.py
+++ rcdb_research/sampling/__init__.py
@@ -3,3 +3,5 @@ from .bootstrap import bootstrap_path, bootstrap_path_2d  # noqa
 
 from .cv import CombinatorialCV, split_indexes_to_bars, predict_splits, predicts_to_paths  # noqa
 from .cv import WalkForwardCV, cross_val_predict_timeseries_splits  # noqa
+
+from .sequential_bootstrap import sequential_bootstrap, average_uniqueness  # noqa
