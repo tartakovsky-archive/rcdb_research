f3a15262af6ca50fa8e2f497bc0ddf09639b6c9b
Ilya Pikulin <ilya.pikulin@gmail.com>
2020-06-20 01:53:25 +0300

    CSSB: sample clusters without replacement

--- rcdb_research/models/sbb.py
+++ rcdb_research/models/sbb.py
@@ -56,7 +56,7 @@ def _generate_bagging_indices(random_state, bootstrap_features, n_features, max_
     else:
         if bootstrap_features is True:
             raise ValueError('case not supported: bootstrap_features=True and clusters_int is not None')
-        clusters_subsample = random_state.choice(clusters_int, size=max_features)
+        clusters_subsample = random_state.choice(clusters_int, size=max_features, replace=False)
         feature_indices = [random_state.choice(columns) for columns in clusters_subsample]
 
     #     sample_indices = seq_bootstrap(ind_mat, sample_length=max_samples, random_state=random_state)  # <- --- ---
