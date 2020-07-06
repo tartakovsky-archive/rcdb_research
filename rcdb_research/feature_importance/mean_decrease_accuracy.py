429367e27090f7b02620f04c49d82681d328bd8d
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-07-06 21:04:14 +0300

    Implement MDA class

--- rcdb_research/feature_importance/mean_decrease_accuracy.py
+++ rcdb_research/feature_importance/mean_decrease_accuracy.py
@@ -1,16 +1,133 @@
 import pandas as pd
 import numpy as np
 import logging
+import inspect
 
 from typing import List, Callable, Optional
 
+# Checks
 from sklearn.metrics import check_scoring
-from sklearn.utils import check_random_state
+from sklearn.utils import check_random_state, check_X_y
+from sklearn.utils.estimator_checks import check_estimator
+from sklearn.model_selection import check_cv
+# ---
+from scipy.stats import rankdata
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.model_selection import BaseCrossValidator
 from tqdm.auto import tqdm
 
 from .utils import cluster_labels_to_clusters
+
+from sklearn.base import BaseEstimator, MetaEstimatorMixin
+
+
+class MDA(MetaEstimatorMixin, BaseEstimator):
+    def __init__(self,
+                 estimator,
+                 scorer,  # sklearn.make_scorer
+                 cv=None,
+                 clusterer: Optional[AgglomerativeClustering] = None,
+                 pooling_fn: Optional[Callable] = None,
+                 n_permutations: int = 10,
+                 random_state=1,
+                 verbose: bool = True):
+        self.estimator = estimator
+        self.scorer = check_scoring(estimator, scorer)
+        self.cv = check_cv(cv)
+        self.clusterer = clusterer
+        self.clusters = None
+        self.pooling_fn = pooling_fn
+        self.n_permutations = n_permutations
+        self.random_state = check_random_state(random_state)
+        self.verbose = verbose
+        self.feature_importances_ = None
+        self.feature_importances_std_ = None
+        self.feature_importances_rank_ = None
+        self.feature_importances_labels_ = None
+        self.feature_importances_df_ = None
+
+    # TODO: test on df and ndarrays
+    def fit(self, X, y, clusters=None, labels=None, score_params=None, **fit_params):
+        score_params = score_params or {}
+        if labels is None:
+            labels = X.columns if hasattr(X, 'columns') else list(range(X.shape[1]))
+        index = X.index if hasattr(X, 'index') else list(range(X.shape[0]))
+        X, y = check_X_y(X, y)
+        X = pd.DataFrame(X, index=index, columns=labels)
+        y = pd.Series(y, index=index)  # refactor if profiler shows it's too slow
+
+        # If either clusterer or clusters are set, then feature clustering would be performad
+        if self.clusterer is not None:
+            if clusters is not None:
+                logging.warning(f'`clusterer` param is set, overriding `clusters` param')
+            self.clusterer.fit(X.T)
+            self.clusters = cluster_labels_to_clusters(self.clusterer.labels_, X.columns)
+        elif clusters is not None:
+            self.clusters = clusters
+        else:
+            self.clusters = [dict(name=col, columns=[col]) for col in X.columns]
+
+        # If both clusters and poolin_fn are set, then feature agglomeration would be performed
+        # Clusters would be merged into single features usign the pooling_fn
+        shouldAgglomerate = self.clusters is not None and self.pooling_fn is not None
+        if shouldAgglomerate:
+
+            agg_X = pd.DataFrame(index=X.index)
+            for i, cluster in enumerate(self.clusters):
+                agg_X[cluster['name']] = self.pooling_fn(X[cluster['columns']])
+                cluster['columns'] = [cluster['name']]
+            X = agg_X
+
+        fit_sample_weight = fit_params.pop('sample_weight', None)
+        score_sample_weight = score_params.pop('sample_weight', None)
+
+        baseline_scores = []  # [n_folds] of floats
+        feature_scores = [[] for _ in self.clusters]  # [n_folds] of [(n_features * n_permutations)]
+
+        # Split data. Show progress bar if verbose
+        splits = self.cv.split(X=X)
+        enumerate_splits = enumerate(tqdm(splits, desc='MDA: processing splits')) if self.verbose else enumerate(splits)
+
+        for i, (train, test) in enumerate_splits:  # for split
+            # Train the model on split's train set
+            if 'clusters' in inspect.getfullargspec(self.estimator.fit).args:
+                self.estimator.fit(X=X.iloc[train], y=y.iloc[train],
+                                   clusters=self.clusters, sample_weight=fit_sample_weight, **fit_params)
+            else:
+                self.estimator.fit(X=X.iloc[train], y=y.iloc[train],
+                                   sample_weight=fit_sample_weight, **fit_params)
+
+            # Get baseline score for split's test set
+            baseline_score = self.scorer(self.estimator, X.iloc[test], y.iloc[test],
+                                         sample_weight=score_sample_weight, **score_params)
+            baseline_scores.append(baseline_score)
+
+            # Get scores for permuted features
+            for j, cluster in enumerate(self.clusters):
+                X_test = X.iloc[test].copy()
+
+                for _ in range(self.n_permutations):
+                    # Permute all features in the cluster
+                    for col in cluster['columns']:
+                        self.random_state.shuffle(X_test[col].values)
+
+                    ft_score = self.scorer(self.estimator, X_test, y.values[test],
+                                           sample_weight=score_sample_weight, **score_params)
+                    feature_scores[j].append(baseline_scores[i] - ft_score)
+
+        self.feature_importances_ = np.mean(feature_scores, axis=1)
+        self.feature_importances_std_ = np.std(feature_scores, axis=1)
+        self.feature_importances_rank_ = rankdata(-self.feature_importances_, method='dense').astype(int)
+        self.feature_importances_labels_ = [c['name'] for c in self.clusters]
+        self.feature_importances_df_ = pd.DataFrame.from_records(
+            np.array([
+                self.feature_importances_,
+                self.feature_importances_std_,
+                self.feature_importances_rank_
+            ]).T,
+            index=self.feature_importances_labels_,
+            columns=['mean', 'std', 'rank']
+        )
 
 
 def mda(estimator,
@@ -62,7 +179,7 @@ def mda(estimator,
             for col in X.columns
         ]
 
-    # If both clustered_subset and poolin_fn is set then feature agglomeration would be performed
+    # If both clusters and poolin_fn are set, then feature agglomeration would be performed
     # Clusters would be merged into single features usign the pooling_fn
     if shouldAgglomerate:
         agg_X = pd.DataFrame(index=X.index)
