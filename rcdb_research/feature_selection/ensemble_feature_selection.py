bc54833c8773811deeccbb39ac970d7eb2ea3641
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-07-09 14:49:37 +0300

    implement EFS draft

--- rcdb_research/feature_selection/ensemble_feature_selection.py
+++ rcdb_research/feature_selection/ensemble_feature_selection.py
@@ -1,24 +1,44 @@
-# from typing import Optional, Union, Any
-#
-# from sklearn.base import BaseEstimator, MetaEstimatorMixin
-# from sklearn.feature_selection import SelectorMixin
-# from sklearn.utils.metaestimators import if_delegate_has_method
-# from sklearn.utils.validation import check_is_fitted
-#
-# from sklearn.feature_selection import SelectFromModel
+import numpy as np
 
-# class EFS(SelectorMixin, MetaEstimatorMixin, BaseEstimator):
-#     def __init__(self,
-#                  selectors: list):
-#         pass
-#
-#     def fit(self, X, y, clusters=None, labels=None, **fit_params):
-#         # Vote
-#         # y_pred_result = np.apply_along_axis(
-#         #     lambda x: np.argmax(np.bincount(x, weights=weights)),
-#         #     axis=0, arr=y_preds
-#         # )
-#         pass
-#
-#     def _get_support_mask(self):
-#         pass
+from typing import Optional
+
+from sklearn.base import BaseEstimator
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.feature_selection import SelectorMixin
+
+from ..feature_importance.checks import check_X_y_labels, check_clusters
+
+from .select_k_best import SelectKBest
+
+
+class EFS(SelectorMixin, BaseEstimator):
+    def __init__(self,
+                 estimators: list,
+                 k_features: int = 10,
+                 clusterer: Optional[AgglomerativeClustering] = None):
+        self.estimators = estimators
+        self.k_features = k_features
+        self.selectors = [SelectKBest(est, k_features=k_features) for est in estimators]
+        self.clusterer = clusterer
+        self.clusters = None
+        self.labels = None
+
+    def fit(self, X, y, clusters=None, labels=None, **fit_params):
+        X, y, labels, index = check_X_y_labels(X, y, labels)
+        self.labels = labels
+        self.clusters = check_clusters(X, self.clusterer, clusters, self.labels)
+
+        for sel in self.selectors:
+            sel.fit(X, y, clusters, labels, **fit_params)
+
+        return self
+
+    def _get_support_mask(self):
+        votes = [sel.get_support() for sel in self.selectors]
+
+        mask = np.apply_along_axis(
+            lambda x: np.argmax(np.bincount(x)),
+            axis=0, arr=votes
+        ).astype(bool)
+
+        return mask
