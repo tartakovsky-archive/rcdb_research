09477196f4c6cea53e6e151201da9604415340fb
tartakovsky <1624603+tartakovsky@users.noreply.github.com>
2020-08-04 16:24:55 +0300

    Implement bounded_relative_log_loss

--- rcdb_research/metrics/prediction.py
+++ rcdb_research/metrics/prediction.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.metrics import log_loss, make_scorer
 
 from ..simulation.probas import Probabilities
+from ..models import NoSkillClassifier
 
 
 def neg_log_loss(y_true: np.ndarray, y_proba: np.ndarray, sample_weight=None) -> float:
@@ -22,6 +23,28 @@ def bounded_log_loss(y_true: np.ndarray,
 
 
 bounded_log_loss_scorer = make_scorer(bounded_log_loss, needs_proba=True)
+
+##################
+# Bounded Relative Log Loss
+##################
+
+def bounded_relative_log_loss(y_true, y_proba, no_skill_score=None, bounds=(0.003, 0.014), sample_weight=None):
+    if no_skill_score is None:
+        X = np.zeros((y_true.size, 2))
+        ns_y_proba = NoSkillClassifier('log_loss').fit(X, y_true).predict_proba(X)
+        no_skill_score = log_loss(y_true, ns_y_proba)
+
+    raw_score = log_loss(y_true, y_proba, sample_weight=sample_weight)
+    relative_score = 1 - raw_score / no_skill_score
+    bounded_score = (relative_score - bounds[0]) / (bounds[1] - bounds[0])
+    return bounded_score
+
+
+bounded_relative_log_loss_scorer = make_scorer(bounded_relative_log_loss, needs_proba=True)
+brll = bounded_relative_log_loss
+brll_scorer = bounded_relative_log_loss_scorer
+
+##################
 
 
 def precision_score_for_activity(activity: float,
